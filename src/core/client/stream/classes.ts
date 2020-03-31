7671c7cbfa9a7e13519724c41fcc4a84b6d0392e
nick-funk <nick.funk@outlook.com>
2020-03-31 11:29:31 -0600

    Update download comments to new branding
    CORL-902

--- src/core/client/stream/classes.ts
+++ src/core/client/stream/classes.ts
@@ -797,6 +797,7 @@ const CLASSES = {
     requestButton: "coral coral-downloadCommentHistory-requestButton",
     recentRequest: "coral coral-downloadCommentHistory-recentRequest",
     requestLater: "coral coral-downloadCommentHistory-requestLater",
+    requestError: "coral coral-downloadCommentHistory-requestError",
   },
 
   /**
